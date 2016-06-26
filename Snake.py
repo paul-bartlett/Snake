4d653c1c3c2a4aec1cac6a85f53fbd69d5c15dc5
Paul Bartlett <bartlett_paul@hotmail.com>
2016-06-25 20:08:14 -0400

    Clean up; added method and variables

--- Snake.py
+++ Snake.py
@@ -1,76 +1,49 @@
 #!/usr/bin/env python
 
-"""
-This is the full and final example from the Pygame Tutorial,
-"How Do I Make It Move". It creates 10 objects and animates
-them on the screen.
-
-Note it's a bit scant on error checking, but it's easy to read. :]
-Fortunately, this is python, and we needn't wrestle with a pile of
-error codes.
-"""
-
-
 #import everything
-import os, pygame
+import pygame, random
 from pygame.locals import *
 
-main_dir = os.path.split(os.path.abspath("__file__"))[0]
+def dead(screen, score):
+    font = pygame.font.SysFont('Arial', 20)
+    post = font.render("Your score was: " + str(score), True, (255, 255, 255))
+    screen.blit(t, (50, 300));
+    pygame.display.update()
+    pygame.time.wait(4000)
 
-#our game object class
-class GameObject:
-    def __init__(self, image, height, speed):
-        self.speed = speed
-        self.image = image
-        self.pos = image.get_rect().move(0, height)
-    def move(self):
-        self.pos = self.pos.move(self.speed, 0)
-        if self.pos.right > 600:
-            self.pos.left = 0
-
-
-#quick function to load an image
-def load_image(name):
-    path = os.path.join(main_dir, 'data', name)
-    return pygame.image.load(path).convert()
-
-
-#here's the full code
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((640, 480))
-
-    # player = load_image('player1.gif')
-    # background = load_image('liquid.bmp')
+    screen = pygame.display.set_mode((640, 640))
+    pygame.display.set_caption("Snake")
+    clock = pygame.time.Clock()
+    framerate = 10;
+    direction = 0;
+    score = 0
+    
     background = pygame.Surface(screen.get_size())
     background = background.convert()
     background.fill((250, 250, 250))
     pygame.draw.rect(screen,(255, 255, 255), [20, 20, 20, 20])
-    # scale the background image so that it fills the window and
-    #   successfully overwrites the old sprite position.
-    # background = pygame.transform.scale2x(background)
-    # background = pygame.transform.scale2x(background)
 
     # screen.blit(background, (0, 0))
 
-    # objects = []
-    # for x in range(10):
-    #     o = GameObject(player, x*40, x)
-    #     objects.append(o)
-
     while 1:
+        clock.tick(framerate);
         for event in pygame.event.get():
-            if event.type in (QUIT, KEYDOWN):
-                return
+            if event.type == QUIT:
+                pygame.quit()
+            elif event.type == KEYDOWN:
+                if event.key == K_UP and direction != 0 : direction = 2
+                elif event.key == K_DOWN and direction != 2 : direction = 0
+                elif event.key == K_LEFT and direction != 1 : direction = 3
+                elif event.key == K_RIGHT and direction != 3 : direction = 1 
+                
         #
         # for o in objects:
         #     screen.blit(background, o.pos, o.pos)
         # for o in objects:
         #     o.move()
         #     screen.blit(o.image, o.pos)
-
-        pygame.display.update()
-
 
 
 if __name__ == '__main__': main()
